9fa7236b0d8fac0496b33cf5cf5246c3e79797a6
Celestia Kai <celestiafan1@gmail.com>
2023-08-27 01:12:50 +0300

    fixed up stuff.
    Signed-off-by: Celestia Kai <celestiafan1@gmail.com>

---
+++ fish/config.fish
@@ -0,0 +1,30 @@
+function fish_greeting
+       # Empty function, no welcome message
+   end
+
+fish_add_path -a "$HOME/.local/bin"
+
+if status is-interactive
+export EDITOR=vim
+export TERM=xterm-256color
+
+alias c='clear'                       # clear
+alias v='vim'                         # vim 
+alias l='ls'                          # list (other variant)
+alias ls='lsd -lh'                    # better list
+alias la='lsd -lha'                   # list all
+alias ll='la'                         # list all (other variant)
+alias df='duf -hide special'          # df -> duf
+alias cat='batcat'                    # cat -> bat
+alias dwm='cd ~/sless/dwm/'           # cd to dwm
+alias kic='cd ~/.config/kitty/'       # cd to kitty
+alias rmpics='rm -rf mpv-shot*'       # remove the compiled up screenshots
+alias weather='curl https://wttr.in/' # weather
+alias nofetch='neofetch --no_config'  # neofetch without any config
+
+# autostart 
+#
+echo "Hello," $(date).
+
+# [ "$(tty)" = '/dev/tty1' ] && exec startx
+end
