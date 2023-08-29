578f860ac4191e911295563e0cb6708e551f92c0
Celestia Kai <celestiafan1@gmail.com>
2023-08-29 04:22:21 +0300

    included ST.
    Signed-off-by: Celestia Kai <celestiafan1@gmail.com>

--- suckless/dwm/config.def.h
+++ suckless/dwm/config.def.h
@@ -31,7 +31,7 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class     instance    title       tags mask     isfloating   monitor */
-	{ "mpv",     NULL,       NULL,       0,            1,           -1 },
+	{ "mpv",     NULL,       NULL,       0,            0,           -1 },
 };
 
 /* layout(s) */
